026fe19686a0db8689da6070867a797601aa12d5
firaz-ahmed <161804312+firaz-ahmed@users.noreply.github.com>
2024-03-19 15:35:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function toggleMenu()
+{
+     var div = document.getElementById("dropdown-menu")
+
+     if(div.style.display=="block")
+     {
+          div.style.display = "none";
+     }
+     else
+     {
+          div.style.display = "block";
+     }
+}
